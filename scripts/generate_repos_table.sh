cb304f95d4e476c9d28d7018ae333d045c9d259e
Hari Sekhon <harisekhon@gmail.com>
2025-02-07 13:54:53 +0700

    updated generate_repos_table.sh

--- scripts/generate_repos_table.sh
+++ scripts/generate_repos_table.sh
@@ -27,6 +27,11 @@ if uname | grep -q Darwin; then
     }
 fi
 
+die(){
+    echo "ERROR: $*"
+    exit 1
+}
+
 echo "Getting Name -> Stars" >&2
 repo_stars="$(
     gh repo list HariSekhon \
@@ -39,23 +44,87 @@ repo_stars="$(
         '
 )"
 
-if [ -f "$local_config" ]; then
-    echo "Reading $local_config" >&2
-    cat "$local_config"
-else
-    echo "Fetching repo list from DevOps-Bash-tools setup/repos.txt" >&2
-    curl -sSf https://raw.githubusercontent.com/HariSekhon/DevOps-Bash-tools/refs/heads/master/setup/repos.txt
-fi |
-sed '
-  s/#.*//;
-  s/:/ /;
-  s/^[[:space:]]*//;
-  s/[[:space:]]*$//;
-  /^[[:space:]]*$/d;
-' |
-grep -v \
-     -e 'harisekhon$' \
-     -e 'lib-java' |
+repo_dirs="$(
+    if [ -f "$local_config" ]; then
+        echo "Reading $local_config" >&2
+        cat "$local_config"
+    else
+        echo "Fetching repo list from DevOps-Bash-tools setup/repos.txt" >&2
+        curl -sSf https://raw.githubusercontent.com/HariSekhon/DevOps-Bash-tools/refs/heads/master/setup/repos.txt
+    fi |
+    sed '
+      s/#.*//;
+      s/:/ /;
+      s/^[[:space:]]*//;
+      s/[[:space:]]*$//;
+      /^[[:space:]]*$/d;
+    ' |
+    grep -v \
+         -e 'harisekhon$' \
+         -e 'lib-java' |
+
+    while read -r repo dir; do
+        if [ -z "$dir" ]; then
+            dir="$repo"
+        fi
+        echo "$repo $dir"
+    done
+)"
+
+tmp="/tmp/github_clean_checkouts"
+
+mkdir -p -v "$tmp"
+
+pushd "$tmp"
+
+echo "Checking out repos" >&2
+while read -r repo dir; do
+    echo "Repo: $repo" >&2
+    if [ -d "$dir" ]; then
+        echo "Pulling Repo: $repo" >&2
+        pushd "$dir"
+        git pull
+        popd
+    else
+        echo "Cloning Repo: $repo" >&2
+        git clone "https://github.com/HariSekhon/$repo" "$dir"
+    fi
+done <<< "$repo_dirs"
+
+echo "Running scc on clean repo checkouts" >&2
+echo >&2
+# --json doesn't output totals
+#scc="$(scc . --format json | jq -M | tee /dev/stderr)"
+scc="$(scc . | tee /dev/stderr)"
+echo >&2
+
+echo "Parsing and Calculating SCC stats" >&2
+total_cost="$(awk '/^Estimated Cost to Develop/{print $NF}' <<< "$scc")"
+echo "Total Cost: $total_cost"
+total_months="$(
+    awk '/^Estimated Schedule Effort/ {print $0}' <<< "$scc" |
+    grep -Eo '[[:digit:].]+ months' |
+    sed 's/ months//; s/[[:space:]]*//' ||
+    die "FAILED to parse man months from SCC output"
+)"
+echo "Total Months: $total_months"
+total_people="$(awk '/^Estimated People Required/ {print $NF}' <<< "$scc")"
+echo "Total People Required: $total_people"
+total_man_months="$(bc -l <<< "$total_people * $total_months" | sed 's/\..*$//')" # discard partial month for simplicity
+echo "Total Man Months: $total_man_months"
+total_man_years_months="$((total_man_months / 12)) years $((total_man_months % 12)) months"
+echo "Total Man Years and Months: $total_man_years_months"
+if ! awk '{print $3}' <<< "$scc" | head -n2 | grep -qi Lines; then
+    die "Lines no longer column 3 in scc output"
+fi
+total_lines="$(awk '/^Total /{print $3}' <<< "$scc")"
+echo "Total Lines: $total_lines"
+echo >&2
+popd
+echo >&2
+
+{
+echo "<table>"
 while read -r repo dir; do
     #echo "Parsing Stars for repo: $repo" >&2
     stars="$(awk "BEGIN { IGNORECASE = 1 } /^$repo / {print \$2; exit}" <<< "$repo_stars")"
@@ -63,22 +132,58 @@ while read -r repo dir; do
         echo "WARNING: no stars parsed for repo: $repo" >&2
     fi
     echo "${stars:-0} $repo $dir"
-done |
+done <<< "$repo_dirs" |
 sort -nr |
-while read -r star repo dir; do
+while read -r stars repo dir; do
     #echo "Generating repo: $repo" >&2
-    if [ -z "$dir" ]; then
-        dir="$repo"
-    fi
     cat <<EOF
-<tr>
-<td> <img src="https://img.shields.io/badge/HariSekhon-${repo//-/--}-blue?logo=github&link=https%3A%2F%2Fgithub.com%2FHariSekhon%2F$repo#readme" /> </td>
-<td> <img src="https://img.shields.io/github/stars/HariSekhon/$repo?logo=github&link=https://github.com/HariSekhon/DevOps-Bash-tools/stargazers" /> </td>
-<td> <img src="https://img.shields.io/github/forks/harisekhon/$repo?logo=github&link=https://github.com/HariSekhon/DevOps-Bash-tools/network" /> </td>
-<td> <a href="https://github.com/HariSekhon/$repo"> <img src="https://sloc.xyz/github/HariSekhon/$repo/?badge-bg-color=2081C2" /> </a> </td>
-<td> <a href="https://github.com/HariSekhon/$repo"> <img src="https://sloc.xyz/github/HariSekhon/$repo/?badge-bg-color=2081C2&category=cocomo" /> </a> </td>
-</tr>
+    <tr>
+        <td>
+            <a href="https://github.com/HariSekhon/$repo#readme">
+                <img src="https://img.shields.io/badge/HariSekhon-${repo//-/--}-blue?logo=github" />
+            </a>
+        </td>
+        <td>
+            <a href="https://github.com/HariSekhon/$repo/stargazers">
+                <img src="https://img.shields.io/github/stars/HariSekhon/$repo?logo=github" />
+            </a>
+        </td>
+        <td>
+            <a href="https://github.com/HariSekhon/$repo/network">
+                <img src="https://img.shields.io/github/forks/harisekhon/$repo?logo=github" />
+            </a>
+        </td>
+        <td>
+            <a href="https://github.com/HariSekhon/$repo">
+                <img src="https://sloc.xyz/github/HariSekhon/$repo/?badge-bg-color=2081C2" />
+            </a>
+        </td>
+        <td>
+            <a href="https://github.com/HariSekhon/$repo">
+                <img src="https://sloc.xyz/github/HariSekhon/$repo/?badge-bg-color=2081C2&category=cocomo" />
+            </a>
+        </td>
+    </tr>
 EOF
-done |
+done
+cat <<EOF
+</table>
+
+<table>
+    <tr>
+        <td>
+            <img src="https://img.shields.io/badge/Total%20Lines-$total_lines-blue" />
+        </td>
+        <td>
+            <img src="https://img.shields.io/badge/Total%20Man%20Years-${total_man_years_months// /%20}-blue" />
+        </td>
+        <td>
+            <img src="https://img.shields.io/badge/Total%20COCOMO-$total_cost-blue" />
+        </td>
+    </tr>
+</table>
+
+EOF
+} |
 tee /dev/stderr |
 copy_to_clipboard.sh  # from DevOps-Bash-tools which should be in the $PATH
